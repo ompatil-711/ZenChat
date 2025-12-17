ec5bf5ac72a4276d1ed3aab0203eb6273b05828b
ompatil-711 <ompatilll.001@gmail.com>
2025-12-17 23:07:23 +0530

    Chat setup

---
+++ BackEnd/Chat/dist/models/chat.d.ts
@@ -0,0 +1,17 @@
+import { Document } from "mongoose";
+import mongoose from "mongoose";
+export interface IChat extends Document {
+    users: string[];
+    latestMessage: {
+        text: string;
+        sender: string;
+    };
+    createdAt: Date;
+    updatedAt: Date;
+}
+export declare const Chat: mongoose.Model<IChat, {}, {}, {}, Document<unknown, {}, IChat, {}, mongoose.DefaultSchemaOptions> & IChat & Required<{
+    _id: mongoose.Types.ObjectId;
+}> & {
+    __v: number;
+}, any, IChat>;
+//# sourceMappingURL=chat.d.ts.map
