51182ae1af0dbed5c15464120ceaa4c573f2b3f2
ompatil-711 <ompatilll.001@gmail.com>
2026-01-01 21:23:06 +0530

    fixes

--- BackEnd/User/dist/controllers/user.js
+++ BackEnd/User/dist/controllers/user.js
@@ -23,7 +23,7 @@ export const loginUser = TryCatch(async (req, res) => {
     });
     const message = {
         to: email,
-        subject: "Your otp code - ZenChat{~by Om Patil}",
+        subject: "Your otp code - ZenChat",
         body: `Your OTP is ${otp}. It is valid for 5 minutes`,
     };
     await publishtoQueue("send-otp", message);
